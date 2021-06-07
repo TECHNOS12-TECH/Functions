3148bb2ebe1f531296ceb1b605b9737c160dfb71
BINARY BEASTS <83653797+TECHNOS12-TECH@users.noreply.github.com>
2021-06-07 19:37:07 +0530

    Convert temperature Celsius to Fahrenheit

---
+++ p2.py
@@ -0,0 +1,9 @@
+# Convert temperature celsius to fahrenheit
+pass
+cel=float(input("Enter temperature in celsius: "))
+pass
+def temp(cel):
+    return ((9/5)*cel)+32
+    pass
+far = temp(cel)
+print("Your temperature in Fahrenheit is:",far)
